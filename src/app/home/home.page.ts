280734ef4760e3a31fa9a699ee43cf23eb4b85fc
Uziel <Uziel.estrada@g-global.com>
2021-10-12 19:29:27 -0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  cuadernos = [
+    {
+      id: 1,
+      descripcion: 'Raya',
+      precio: 243,
+      marca:'Mead',
+      image: 'cuaderno.jpg',
+      rank:5
+    },
+    {
+      id: 2,
+      descripcion: 'cuadro',
+      precio: 223,
+      marca:'Mead',
+      image: 'cuaderno.jpg',
+      rank:5
+    }
 
+  ]
   constructor() {}
 
 }
