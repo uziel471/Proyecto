a93c47fd746034ed4aa424b5d9592c6e9f7937e8
Uziel <Uziel.estrada@g-global.com>
2021-11-07 19:00:49 -0800

    add method createList

--- src/app/listas/create/create.page.ts
+++ src/app/listas/create/create.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, Type } from '@angular/core';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.page.html',
   styleUrls: ['./create.page.scss'],
 })
-export class CreatePage implements OnInit {
+export class CreatePage{
 
-  constructor() { }
-
-  ngOnInit() {
+  nombre: string;
+  constructor(private http: HttpClient) { }
+  createdList() {
+    const postData = {
+      namePerson: this.nombre,
+      usuarioId: 1
+    };
+    this.http.post('http://localhost:4000/lista/lista/create', postData).subscribe((res: any) => {
+      console.log(res);
+    });
   }
 
 }
