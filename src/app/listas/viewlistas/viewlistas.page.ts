4c84ecc4534eec7d46d9bae886563a5ee156d3f9
Uziel <Uziel.estrada@g-global.com>
2021-11-21 16:40:01 -0800

    add listview

---
+++ src/app/listas/viewlistas/viewlistas.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewlistas',
+  templateUrl: './viewlistas.page.html',
+  styleUrls: ['./viewlistas.page.scss'],
+})
+export class ViewlistasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
