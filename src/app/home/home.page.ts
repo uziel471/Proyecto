5d6f651c3d71980b08dc33458c3e8dc7c2ab2590
RTercero GÃomez Roman <roman3t@hotmail.com>
2021-12-05 21:14:47 -0800

    agregando redirección de paginas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { from } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
-  constructor() {}
+//  session = "xd"
+
+  constructor(public router:Router) {}
+
+  ionViewWillEnter(){
+    this.login();  
+  }
+
+  login(){
+    console.log('Comprobando');
+//    window.localStorage.setItem('sessin', this.session);
+
+    if(!window.localStorage.getItem('@session')){
+      this.redirectOther();
+      console.log('no esta')
+    }
+  }
+
+  redirectOther(){
+    this.router.navigateByUrl('/login')
+  }
 
 }
